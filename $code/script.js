966cc23df251f8af8c069d7fba662e9d1df6b36e
mplotus <mplotus.github.io@gmail.com>
2022-12-01 15:27:40 +0700

    Update src of body iframe

--- $code/script.js
+++ $code/script.js
@@ -1,11 +1,11 @@
 const nav_btt_click = event => {
     let _bId = 2;
     switch(event.currentTarget.title) {
-        case 'Security': { _bId = 0; break; }
-        case 'Terabox': { _bId = 1; break; }
-        case 'Google Drive': { _bId = 2; break; }
-        case 'Web Applications': { _bId = 3; break; }
-        case 'Tips': { _bId = 4; break; }
+        case 'Security': _bId = 0; break; 
+        case 'Terabox': _bId = 1; break;
+        case 'Google Drive': _bId = 2; break;
+        case 'Web Applications': _bId = 3; break;
+        case 'Tips': _bId = 4; break;
         default: _bId = 2;
     }
     let _btts = document.getElementsByClassName('nav_btt');
@@ -17,4 +17,22 @@ const nav_btt_click = event => {
     _btts[_bId].style.marginTop = '-14px';
     _btts[_bId].style.backgroundColor = '#191919';
     _btts[_bId].children[1].style.opacity = '1';
+    switch(_bId) {
+        case 0: 
+            document.getElementById('body_page').src = './accounts/index.html';
+            break;
+        case 1: 
+            document.getElementById('body_page').src = './t/index.html';
+            break;
+        case 2: 
+            document.getElementById('body_page').src = './g/index.html';
+            break;
+        case 3:
+            document.getElementById('body_page').src = './webapps/index.html';
+            break;
+        case 4:
+                document.getElementById('body_page').src = 'https://mplotus.github.io/tips';
+                break;
+        default: ;
+    }
 }
